0067def5fbd8d9b72b89fe15af1603d744b062b4
Pratik Basnet <iampratikbasnet@gmail.com>
2024-06-03 15:43:55 +0545

    Add User Functionality

--- Error Handling ⚠️/index.js
+++ Error Handling ⚠️/index.js
@@ -1,25 +1,37 @@
 
 
-try {
-    const dividend = Number(window.prompt("Enter divident: "));
-    const divisor = Number(window.prompt("Enter divisor: "));
+function divison() {
 
-    if (divisor == 0) {
-        throw new Error("You can't divide by zero.");
+    try {
+        const dividend = Number(window.prompt("Enter divident: "));
+        const divisor = Number(window.prompt("Enter divisor: "));
+
+        if (divisor == 0) {
+            throw new Error("You can't divide by zero.");
+        }
+
+        if (isNaN(dividend) || isNaN(divisor)) {
+            throw new Error("Values must be a number.");
+        }
+
+        const result = dividend / divisor;
+        alert(`The result of the division is ${result}.`);
     }
 
-    if (isNaN(dividend) || isNaN(divisor)) {
-        throw new Error("Values must be a number.");
+    catch (error) {
+        console.error(error);
     }
 
-    const result = dividend / divisor;
-    console.log(`The result of the division is ${result}`);
+    finally {
+        const ask = prompt("Try Again? [Y/N]").toUpperCase();
+
+        if (ask.includes("Y")) {
+            divison();
+        }
+        else {
+        }
+
+    }
 }
 
-catch (error) {
-    console.error(error);
-}
-
-finally {
-    console.log("Try Again?")
-}
+divison();
